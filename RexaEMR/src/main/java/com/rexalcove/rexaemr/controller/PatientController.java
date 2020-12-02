8e438c8d78471071c090051f4ef6175210e5f5a0
rex77 <ahnyeawon77@gmail.com>
2020-12-02 15:24:07 +0900

    Patient Mapper Modified

--- RexaEMR/src/main/java/com/rexalcove/rexaemr/controller/PatientController.java
+++ RexaEMR/src/main/java/com/rexalcove/rexaemr/controller/PatientController.java
@@ -1,5 +1,7 @@
 package com.rexalcove.rexaemr.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import org.json.JSONArray;
@@ -11,7 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.rexalcove.rexaemr.dto.DoctorDTO;
 import com.rexalcove.rexaemr.dto.PatientDTO;
 import com.rexalcove.rexaemr.service.PatientService;
 import com.rexalcove.rexaemr.util.ResultData;
@@ -65,8 +66,8 @@ public class PatientController {
 		try {
 			patient = patientService.getPatient(idx);
 			resultData.setHeader("200", "OK");
-			
-			if(patient == null) {
+
+			if (patient == null) {
 				resultData.setHeader("404", "Given Patient Not Found");
 				patient = new PatientDTO();
 			}
@@ -80,12 +81,14 @@ public class PatientController {
 
 	@ApiOperation(value = "환자 삽입", notes = "특정 환자를 삽입합니다.")
 	@PostMapping("/insertPatient")
-	public String insertPatient(PatientDTO patient) {
+	public String insertPatient(String name, String email, String contact, String dob, String insurance,
+			String gender) {
 		// 변수 선언
 		ResultData resultData = new ResultData();
 		// 로직 처리
 		try {
-			patientService.insertPatient(patient.getName(), patient.getEmail(), patient.getContact(),patient.getDob(), patient.getInsurance(), patient.getGender());
+			Date dobd = new SimpleDateFormat("yyyy-mm-dd").parse(dob);
+			patientService.insertPatient(name, email, contact, dobd, insurance, gender);
 			resultData.setHeader("200", "OK");
 		} catch (Exception e) {
 			resultData.setHeader("500", "Internal Server Error");
@@ -97,13 +100,42 @@ public class PatientController {
 
 	@ApiOperation(value = "환자 삭제", notes = "특정 환자를 삭제합니다.")
 	@DeleteMapping("/deletePatient")
-	public String deletePatient() {
-		return null;
+	public String deletePatient(int idx) {
+		// 변수 선언
+		ResultData resultData = new ResultData();
+		// 로직 처리
+		try {
+			boolean result = patientService.deletePatient(idx);
+			if (result)
+				resultData.setHeader("200", "OK");
+			else
+				resultData.setHeader("204", "Patient Not Found");
+		} catch (Exception e) {
+			resultData.setHeader("500", "Internal Server Error");
+			e.printStackTrace();
+		}
+		return resultData.getResultData().toString();
 	}
 
 	@ApiOperation(value = "환자 정보 업데이트", notes = "환자 정보를 업데이트합니다.")
 	@PostMapping("/updatePatient")
-	public String updatePatient() {
-		return null;
+	public String updatePatient(int idx, String name, String email, String contact, String dob, String insurance,
+			String gender) {
+		// 변수 선언
+				ResultData resultData = new ResultData();
+				// 로직 처리
+				try {
+					Date dobd = new SimpleDateFormat("yyyy-MM-dd").parse(dob);
+					boolean result = patientService.updatePatient(idx, name, email, contact, dobd, insurance, gender);
+					if (result)
+						resultData.setHeader("200", "OK");
+					else
+						resultData.setHeader("404", "Patient Not Found");
+				} catch (Exception e) {
+					resultData.setHeader("500", "Internal Server Error");
+					e.printStackTrace();
+				}
+
+				return resultData.getResultData().toString();
 	}
 }
